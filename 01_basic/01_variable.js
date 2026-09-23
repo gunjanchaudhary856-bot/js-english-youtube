3e37ef80c3b1c3215c6b9f9999f11d8a4d907f21
Gunjan Chaudhary <gunjanchaudhary856@gmail.com>
2026-09-23 00:51:34 +0000

    01_variable

---
+++ 01_basic/01_variable.js
@@ -0,0 +1,10 @@
+const accountid = 144132
+let accountemail= "chaudharygunjan315@hmail.com"
+var accountpassword = "12345"
+accountcity = "jattari"
+let accountstate;
+// const accountid: 
+ accountemail= "hc@hc.com"
+ accountcity = "GUJRAT"
+console.log(accountid);
+console.table([accountid, accountemail, accountpassword, accountcity,accountstate])
