c8665f610501142314caed0359118823919ffb04
Gunjan Chaudhary <gunjanchaudhary856@gmail.com>
2026-09-23 01:03:49 +0000

    sucsefully

--- 01_basic/01_variable.js
+++ 01_basic/01_variable.js
@@ -8,3 +8,21 @@ let accountstate;
  accountcity = "GUJRAT"
 console.log(accountid);
 console.table([accountid, accountemail, accountpassword, accountcity,accountstate])
+/*
+Quick Revision
+Keyword / Method	     Meaning
+const	                 Value ko reassign nahi kar sakte
+let	                     Value ko reassign kar sakte
+var	                     Variable declare karta hai; modern JS me let/const preferred
+console.log()	         Output print karta hai
+console.table()	         Data ko table form me dikhata hai
+undefined	             Variable declared hai but value assign nahi hui
+No datatype needed	     JavaScript dynamically typed hai
+
+
+⭐ Easy Trick
+const → Change nahi
+let   → Change kar sakte
+var   → Old way, generally avoid in modern JS
+
+*/
